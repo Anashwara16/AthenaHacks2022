34133ab950842a1ea9540ea284145f8b68621258
Anashwara <73360159+Anashwara16@users.noreply.github.com>
2022-06-09 15:22:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+/*--ATHENAHACKS 2022
+    Feburary 20, 2022
+    (main.js)
+*/
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
